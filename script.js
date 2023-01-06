9a27d01fe5df817cf91c6d1abe63b0e24021c1b6
Ammar Hasan <ammarhasan011@gmail.com>
2023-01-06 22:15:03 +0100

    a

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-const form = document.querySelector('tvform');
+const form = document.querySelector('form');
 
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     //att inte html uppdaterr sig
-});
-
-const formData = new FormData(form);
 
 
-for (item of formData) {
-    console.log(item[0], item[1]);
-}
+    const formData = new FormData(form);
 
-fetch('http://localhost:3000/api/products', {
-    method: "POST",
-    body: formData,
-})
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .then(res => console.log(res));
+    const res = Object.fromEntries(formData)
+
+    const payload = JSON.stringify(res)
+
+    console.log(payload)
+
+    //visar i konsolen
+    for (item of formData) {
+        console.log(item[0], item[1]);
+    };
+
+
+
+    fetch('http://localhost:3000/api/products', {
+        method: "POST",
+        body: payload,
+        headers: {
+            'Content-Type': "application/json"
+        }
+    })
+
+
+}); 
