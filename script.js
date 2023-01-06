d5a6b49e5e41550e543e591509eccb2511fe7487
Ammar Hasan <ammarhasan011@gmail.com>
2023-01-06 17:44:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const form = document.querySelector('#form');
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const formdata= new formdata1();
+})
+
+// const data ={
+    //     name:
+    // }
+
+
+    
+    // fetch('https://localhost:3000/api/products/3', method: 'post')
